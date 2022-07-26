7ca20c2b0b55b4d942866aa86c9449bf4fbaa5b9
AJ <74534547+aj-in@users.noreply.github.com>
2022-07-27 00:20:30 +0530

    Update linearSearch.py

--- SearchingAlgos/linearSearch.py
+++ SearchingAlgos/linearSearch.py
@@ -1,9 +1,9 @@
 def search(list1,num):
     for i in range (len(list1)):         #need a number that increments , thus for loop till the end of list [len(list1) -1 not used ,yea ik we 					
-                                         #want index which is len-1 BUT range already skips the last elment] 
+                                         #want index which is len-1 BUT range already skips the last element] 
         if list1[i]==num:    
             print(f" number found at index {i} ")
-            break
+            break                                  # this breaks out of loop if num found ,  n doesnt execute else block
         
     else:
         print(" num not found in array/list")
@@ -18,3 +18,23 @@ search(list1 , num )
 
 
 
+# simpler method
+
+# list1=[23,3,11,24,63]
+
+# num = 1
+
+# for i in list1:
+#     if num == i:
+#         print(f"num found at {list1.index(i)} index" )           # list_name . index (index_no.)   # this returns the index val
+#         break                              # this wont search for others if num found  ,does this break out of every indent?
+    
+# else:
+#     print("num not found")         # why wont this be executed after every unsuccesfull attempt?
+    
+                      
+    
+    
+ 
+
+    
