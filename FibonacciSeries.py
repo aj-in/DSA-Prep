adf74063ffb909d9f14bcae5628c6238396b0788
AJ <74534547+aj-in@users.noreply.github.com>
2022-11-17 19:59:29 +0530

    Update FibonacciSeries.py

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -7,3 +7,20 @@ for i in range(n):
     first=second
     second=temp+second
     
+    
+#     OR
+
+def fib(n):
+    
+    
+    if n==1:
+        return 0
+    if n==2:
+        return 1
+        
+    if n>2:
+        return fib(n-1)+fib(n-2)             // sum of last 2 num
+        
+for n in range(1,3):                 // change this value
+    
+    print(n , ":" ,fib(n))
