03fb895bbc08ce6f861e6ee7cb3982b20932265a
AJ <74534547+aj-in@users.noreply.github.com>
2023-01-20 03:23:04 +0530

    Add files via upload

---
+++ Palindrome.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+std::string is_palindrome(std::string text){
+
+std::string rev_str;
+
+for (int i=text.size()-1 ; i>=0 ; i--){     // len() - 1, cuz index is 1 less than len, i>=0 cuz 0 is the min
+  rev_str += text[i];
+}
+
+
+if( rev_str == text){
+  return "text is palindrome";
+}
+
+else{
+  return "text is not a palindrome";
+}
+
+}
+
+int main() {
+  
+  std::cout << is_palindrome("madam") << "\n";
+  std::cout << is_palindrome("ada") << "\n";
+  std::cout << is_palindrome("lovelace") << "\n";
+  
+}
